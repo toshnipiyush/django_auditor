afb39ce7f7bf8ae60bcc90ea192b365a33e29fa9
toshnipiyush <piyush.toshniwal@hashedin.com>
2019-04-02 16:30:22 +0530

    Updated admin search field

--- django_auditor/admin.py
+++ django_auditor/admin.py
@@ -8,7 +8,7 @@ from django_auditor.models import EntityAuditLog
 class EntityAuditLogAdmin(admin.ModelAdmin):
     list_display = ('id', 'fm_entity_type', 'fk_entity_id', 'attribute', 'old_value', 'new_value', 'performed_by',
                     'operation_type', 'created_on', 'comments',)
-    search_fields = ('id', 'attribute', 'old_value', 'new_value', 'performed_by__phone_number',)
+    search_fields = ('id', 'attribute', 'old_value', 'new_value', 'performed_by__username',)
     list_filter = ('operation_type', 'fm_entity_type',)
 
     class Meta:
